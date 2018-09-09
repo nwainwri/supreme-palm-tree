430a6756c2b1643e401c115e245e2412e56e5645
nwainwri <nathanwainwright@gmail.com>
2018-09-08 22:51:31 -0700

    everything up until Working with View Controllers done

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,39 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Nathan Wainwright on 2018-09-08.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+//MARK: Initialization
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    
+    
+    
+  }
+  
+  required init(coder: NSCoder) {
+//    fatalError("init(coder:) has not been implemented")
+    super.init(coder: coder)
+  }
+  
+  
+  
+  
+  
+  
+}
