4a92c8fff77d6695174f254ae8504649a10738f5
nwainwri <nathanwainwright@gmail.com>
2018-09-09 00:09:59 -0700

    defined data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,43 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Nathan Wainwright on 2018-09-09.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+  
+  //MARK: Properties
+  
+  var name: String
+  var photo: UIImage?
+  var rating: Int
+  
+  //MARK: Initializatio
+  init?(name: String, photo: UIImage?, rating: Int) {
+    
+    // The name must not be empty
+    guard !name.isEmpty else {
+      return nil
+    }
+    
+    // The rating must be between 0 and 5 inclusively
+    guard (rating >= 0) && (rating <= 5) else {
+      return nil
+    }
+    
+    
+    // Initialize stored properties.
+    self.name = name
+    self.photo = photo
+    self.rating = rating
+  }
+  
+}
+
+
+
+
