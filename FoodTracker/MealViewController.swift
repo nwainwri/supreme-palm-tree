92ae867c6eb51499b2dc2043b8eca3ef65ff178a
nwainwri <nathanwainwright@gmail.com>
2018-09-09 01:38:00 -0700

    added Table View

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   //MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
-  @IBOutlet weak var mealNameLabel: UILabel!
   @IBOutlet weak var photoImageView: UIImageView!
   @IBOutlet weak var ratingControl: RatingControl!
   
@@ -36,7 +35,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
   }
   
   func textFieldDidEndEditing(_ textField: UITextField) {
-    mealNameLabel.text = textField.text
+
+    
   }
   
   //MARK: UIImagePickerControllerDelegate
